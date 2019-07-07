2c5953fe4960ce204127f724ff908fa99cee4ec6
likaipeng <lkpTomorrow@gmail.com>
2019-07-07 11:41:50 +0800

    引入antd，并体验button，icon，组件默认语言，并切换组件默认语言的问题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NzI18nService,zh_CN,en_US} from "ng-zorro-antd";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ksbk';
+
+/**
+ * 测试modal组件的语言：
+ */
+  isVisible = false;
+
+  constructor(
+    private nzI18nService: NzI18nService,
+  ) {}
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+
+  /**
+   * 切换语言
+   */
+  changeLang(lang){
+    if(lang=='en'){
+      this.nzI18nService.setLocale(en_US);
+    }else {
+      this.nzI18nService.setLocale(zh_CN);
+    }
+  }
 }
